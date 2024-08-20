1e915da83f592da60972e8c6100d7b072fcc1cdb
= <pete.buo@gmail.com>
2024-08-19 23:53:39 -0400

    dynamic font

--- source_code/my-client/src/Components/ProjectCard.jsx
+++ source_code/my-client/src/Components/ProjectCard.jsx
@@ -1,41 +1,64 @@
-import React from "react";
+import React, { useRef, useEffect, useState } from "react";
 
-const ProjectCard = props => {
+const ProjectCard = (props) => {
+    const cardBorderRef = useRef(null);
+    const [fontSize, setFontSize] = useState("1rem"); // Default font size
 
-    if (props.project)
-    {
+    useEffect(() => {
+        const updateFontSize = () => {
+            if (cardBorderRef.current) {
+                const cardBorderWidth = cardBorderRef.current.offsetWidth;
+                const calculatedFontSize = (cardBorderWidth * 0.03) + 1 + "px"; // Example: 5% of card-border width
+                setFontSize(calculatedFontSize);
+            }
+        };
 
-        function view()
-        {
-            props.view(props.project)
+        updateFontSize(); // Initial calculation
+        window.addEventListener("resize", updateFontSize); // Recalculate on window resize
+
+        return () => {
+            window.removeEventListener("resize", updateFontSize); // Clean up event listener
+        };
+    }, []); // Empty dependency array
+
+    const view = () => {
+        if (props.project) {
+            props.view(props.project);
         }
-        
-        return( 
-            <div id = "card-border" onClick = {view}> 
-                
-                <div className="card border-secondary mb-3" id = "card">
-                    <div className="card-header">{(props.project.title)? props.project.title : "Project Name"}</div>
+    };
+
+    if (props.project) {
+        return (
+            <div id="card-border" ref={cardBorderRef} onClick={view}>
+                <div className="card border-secondary mb-3" id="card">
+                    <div className="card-header">
+                        {props.project.title ? props.project.title : "Project Name"}
+                    </div>
                     <div className="card-body text-secondary">
-                        <div id = "project-preview">
+                        <div id="project-preview">
                             {/* eslint-disable-next-line */}
-                            <img src = {props.project.icon} alt = "Loading Image..." width = "150px" height = "150px" id = "card-img"
-                            onError={event => {
-                                // Load the default image
-                                event.target.src = "default-img.jpg"
-                                event.onerror = null
-                            }}></img>
-                            {/* Short description */}
-                            <p style={{marginLeft: "10px"}}>{props.project.description}</p>
+                            <img
+                                src={props.project.icon}
+                                alt="Loading Image..."
+                                width="150px"
+                                height="150px"
+                                id="card-img"
+                                onError={(event) => {
+                                    event.target.src = "default-img.jpg";
+                                    event.onerror = null;
+                                }}
+                            />
+                            <p id="card-desc" style={{ marginLeft: "10px", fontSize: fontSize }}>
+                                {props.project.description}
+                            </p>
                         </div>
                     </div>
                 </div>
-        
             </div>
-            )
-        
-    
+        );
     }
 
-}
+    return null;
+};
 
 export default ProjectCard;
